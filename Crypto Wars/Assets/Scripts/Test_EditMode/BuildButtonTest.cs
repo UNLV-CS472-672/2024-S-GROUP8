2d1a04f2f8ecc71536b25260e139a81655135cd9
Gnarizard <toefoo@protonmail.com>
2024-04-23 18:58:15 -0700

    Modified the setup method to ensure the buildButton filed != Null

--- Crypto Wars/Assets/Scripts/Test_EditMode/BuildButtonTest.cs
+++ Crypto Wars/Assets/Scripts/Test_EditMode/BuildButtonTest.cs
@@ -7,20 +7,25 @@ public class BuildButtonTest
     public GameObject go;
     public BuildButtonScript buildButton;
 
-
     [SetUp]
     public void SetUp()
     {
+        // Create the main GameObject that the BuildButtonScript is attached to
         go = new GameObject("Build Button");
         go.AddComponent<BuildButtonScript>();
         buildButton = go.GetComponent<BuildButtonScript>();
+
+        // Create a mock build button GameObject and assign it
+        var mockBuildButton = new GameObject("Mock Build Button");
+        buildButton.buildButton = mockBuildButton;
+
         go.transform.position = new Vector3(0, 0, 0);
     }
 
     [Test]
     public void TestOutOfFrame()
     {
-        Vector3 expectedPosition = new Vector3(0,-375,0);
+        Vector3 expectedPosition = new Vector3(0, -375, 0);
         buildButton.DeactivateMain();
         Assert.AreEqual(expectedPosition, go.transform.position);
     }
