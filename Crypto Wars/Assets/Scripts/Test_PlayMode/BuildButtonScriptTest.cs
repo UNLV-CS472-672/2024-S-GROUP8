e67a6324c16f3c286789833309a02bb44458494b
Gnarizard <toefoo@protonmail.com>
2024-04-23 20:20:35 -0700

    Safely exsposed private objects via properties that were breaking test cases.

--- Crypto Wars/Assets/Scripts/Test_PlayMode/BuildButtonScriptTest.cs
+++ Crypto Wars/Assets/Scripts/Test_PlayMode/BuildButtonScriptTest.cs
@@ -17,33 +17,19 @@ public class BuildButtonScriptTest
 
         buildButton = new GameObject("BuildButton");
         buildButton.transform.SetParent(canvas.transform);
-        buildButton.AddComponent<Image>();  // Image component??
-
-        GameObject buildText = new GameObject("BuildButtonText");
-        buildText.transform.SetParent(buildButton.transform);
-        buildText.AddComponent<Text>();
 
         destroyButton = new GameObject("DestroyButton");
         destroyButton.transform.SetParent(canvas.transform);
-        destroyButton.AddComponent<Image>();
-
-        GameObject destroyText = new GameObject("DestroyButtonText");
-        destroyText.transform.SetParent(destroyButton.transform);
-        destroyText.AddComponent<Text>();
 
         cancelButton = new GameObject("CancelButton");
         cancelButton.transform.SetParent(canvas.transform);
-        cancelButton.AddComponent<Image>();
-
-        GameObject cancelText = new GameObject("CancelButtonText");
-        cancelText.transform.SetParent(cancelButton.transform);
-        cancelText.AddComponent<Text>();
 
         go.AddComponent<BuildButtonScript>();
         testBuildButton = go.GetComponent<BuildButtonScript>();
-        testBuildButton.buildButton = buildButton;
-        testBuildButton.destroyButton = destroyButton;
-        testBuildButton.cancelButton = cancelButton;
+
+        // Use the public properties to assign mock objects to script
+        typeof(BuildButtonScript).GetProperty("BuildButton").SetValue(testBuildButton, buildButton, null);
+        typeof(BuildButtonScript).GetProperty("DestroyButton").SetValue(testBuildButton, destroyButton, null);
     }
 
     [Test]
