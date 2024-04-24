9ad5fbda913ea0e6ef3be61e441cc6963e03e020
Grimmblood <grimmblood@gmail.com>
2024-04-23 19:55:27 -0700

    attmpting to fix merge conflicts pt. 2

--- Crypto Wars/Assets/Scripts/Test_PlayMode/BuildButtonScriptTest.cs
+++ Crypto Wars/Assets/Scripts/Test_PlayMode/BuildButtonScriptTest.cs
@@ -17,7 +17,6 @@ public class BuildButtonScriptTest
     [SetUp]
     public void SetUp()
     {
-
         go = new GameObject("BuildMenu");
         canvas = new GameObject("Canvas");
         go.AddComponent<BuildButtonScript>();
@@ -32,49 +31,47 @@ public class BuildButtonScriptTest
         */
     }
 
-    [Test]
+    [Test]  //  Currently Broken (Null Reference, BuildButtonScript: 41)
     public void TestToggleMenu()
     {
-
         // buildButton.AddComponent<Image>();
         //testBuildButton.ToggleMenu();
         /*
         Assert.IsFalse(testBuildButton.buildButton.GetComponent<Image>().enabled);
         //testBuildButton.ToggleMenu();
         Assert.IsTrue(testBuildButton.buildButton.GetComponent<Image>().enabled);
-
         */
     }
 
     [Test]
     public void TestCreateBuildButton()
     {
-        // GameObject buildText = GameObject.Find("BuildButtonText");
-        // Assert.IsNotNull(buildText);
+        GameObject buildText = GameObject.Find("BuildButtonText");
+        Assert.IsNotNull(buildText);
     }
 
     [Test]
     public void TestCreateDestroyButton()
     {
-        // GameObject destroyText = GameObject.Find("DestroyButtonText");
-        // Assert.IsNotNull(destroyText);
+        GameObject destroyText = GameObject.Find("DestroyButtonText");
+        Assert.IsNotNull(destroyText);
     }
 
     [Test]
     public void TestCreateCancelButton()
     {
-        // GameObject cancelText = GameObject.Find("CancelButtonText");
-        // Assert.IsNotNull(cancelText);
+        GameObject cancelText = GameObject.Find("CancelButtonText");
+        Assert.IsNotNull(cancelText);
     }
 
     [TearDown]
     public void TearDown()
     {
         // Clean up after each test
-        // Object.DestroyImmediate(cancelButton);
-        // Object.DestroyImmediate(destroyButton);
-        // Object.DestroyImmediate(buildButton);
-        // Object.DestroyImmediate(canvas);
-        // Object.DestroyImmediate(go);
+        Object.DestroyImmediate(cancelButton);
+        Object.DestroyImmediate(destroyButton);
+        Object.DestroyImmediate(buildButton);
+        Object.DestroyImmediate(canvas);
+        Object.DestroyImmediate(go);
     }
 }
